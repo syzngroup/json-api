0ffd51973abb541c1099778d159d71465caecddd
Arad Zaltsberger <arad@syzn.co.il>
2019-02-14 11:17:28 +0200

    Create a DataInterface

---
+++ src/Contracts/DataInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Syzn\JsonApi\Contracts;
+
+use Syzn\JsonApi\Contracts\EncodableJsonApiStructureInterface;
+
+interface DataInterface extends EncodableJsonApiStructureInterface
+{
+    //
+}
