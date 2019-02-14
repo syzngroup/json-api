5778cb9591f363ed176a4d1d16d69c7d7432dca3
Arad Zaltsberger <arad@syzn.co.il>
2019-02-14 13:23:34 +0200

    Implement getMeta

--- tests/Assets/ExampleError.php
+++ tests/Assets/ExampleError.php
@@ -3,16 +3,15 @@
 namespace Syzn\JsonApi\Tests\Assets;
 
 use Syzn\JsonApi\Error;
-
+use Syzn\JsonApi\Contracts\MetaInterface;
+use Syzn\JsonApi\Contracts\Links\LinksInterface;
 use Syzn\JsonApi\Factories\Links\LinkFactory;
 use Syzn\JsonApi\Factories\Links\LinksFactory;
-
-use Syzn\JsonApi\Contracts\Links\LinksInterface;
 
 class ExampleError extends Error
 {
     /**
-     * Retrieve unique error identifier
+     * Retrieve unique error identifier.
      *
      * @return mixed
      */
@@ -22,9 +21,9 @@ class ExampleError extends Error
     }
 
     /**
-     * Retrieve links instance related to the error
+     * Retrieve links instance related to the error.
      *
-     * @return Syzn\JsonApi\Contracts\Links\LinksInterface
+     * @return \Syzn\JsonApi\Contracts\Links\LinksInterface|null
      */
     public function getLinks(): ?LinksInterface
     {
@@ -34,7 +33,7 @@ class ExampleError extends Error
     }
 
     /**
-     * Retrieve the http status code applicable to the error
+     * Retrieve the http status code applicable to the error.
      *
      * @return int|null
      */
@@ -44,7 +43,7 @@ class ExampleError extends Error
     }
 
     /**
-     * Retrieve application-specific code applicable to the error
+     * Retrieve application-specific code applicable to the error.
      *
      * @return int|null
      */
@@ -54,7 +53,7 @@ class ExampleError extends Error
     }
 
     /**
-     * Retrieve short, human-readable summary of the error
+     * Retrieve short, human-readable summary of the error.
      *
      * @return string|null
      */
@@ -64,12 +63,22 @@ class ExampleError extends Error
     }
 
     /**
-     * Retrieve human-readable explanation of the error
+     * Retrieve human-readable explanation of the error.
      *
      * @return string|null
      */
     public function getDetail(): ?string
     {
         return "Explained example error";
+    }
+
+    /**
+     * Retrieve meta object containing non-standard meta-information about the error.
+     *
+     * @return \Syzn\JsonApi\Contracts\MetaInterface|null
+     */
+    public function getMeta(): ?MetaInterface
+    {
+        return null;
     }
 }
