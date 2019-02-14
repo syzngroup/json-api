f55666af15b73ac9a56c2dc2b106c5c011650645
Arad Zaltsberger <arad@syzn.co.il>
2019-02-14 11:39:49 +0200

    ResourceIdentifier now extends BaseResouce and was moved to Resources folder

---
+++ src/Resources/ResourceIdentifier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Syzn\JsonApi\Resources;
+
+use Syzn\JsonApi\Contracts\Resources\ResourceIdentifierInterface;
+use Syzn\JsonApi\Resources\BaseResource;
+
+abstract class ResourceIdentifier extends BaseResource implements ResourceIdentifierInterface
+{
+    //
+}
