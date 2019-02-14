3f0e13c4a279b933a17eb1b70535308ff3e8d1bf
Arad Zaltsberger <arad@syzn.co.il>
2019-02-14 12:57:30 +0200

    Add getMeta

--- src/Contracts/ErrorInterface.php
+++ src/Contracts/ErrorInterface.php
@@ -2,52 +2,60 @@
 
 namespace Syzn\JsonApi\Contracts;
 
-use Syzn\JsonApi\Contracts\Links\LinksInterface;
 use Syzn\JsonApi\Contracts\EncodableJsonApiStructureInterface;
+use Syzn\JsonApi\Contracts\MetaInterface;
+use Syzn\JsonApi\Contracts\Links\LinksInterface;
 
 interface ErrorInterface extends EncodableJsonApiStructureInterface
 {
     /**
-     * Retrieve unique error identifier
+     * Retrieve unique error identifier.
      *
      * @return mixed|null
      */
     public function getId();
 
     /**
-     * Retrieve links instance related to the error
+     * Retrieve links instance related to the error.
      *
-     * @return Syzn\JsonApi\Contracts\Links\LinksInterface|null
+     * @return \Syzn\JsonApi\Contracts\Links\LinksInterface|null
      */
     public function getLinks(): ?LinksInterface;
 
     /**
-     * Retrieve the http status code applicable to the error
+     * Retrieve the http status code applicable to the error.
      *
      * @return int|null
      */
     public function getStatus(): ?int;
 
     /**
-     * Retrieve application-specific code applicable to the error
+     * Retrieve application-specific code applicable to the error.
      *
      * @return int|null
      */
     public function getCode(): ?int;
 
     /**
-     * Retrieve short, human-readable summary of the error
+     * Retrieve short, human-readable summary of the error.
      *
      * @return string|null
      */
     public function getTitle(): ?string;
 
     /**
-     * Retrieve human-readable explanation of the error
+     * Retrieve human-readable explanation of the error.
      *
      * @return string|null
      */
     public function getDetail(): ?string;
+
+    /**
+     * Retrieve meta object containing non-standard meta-information about the error.
+     *
+     * @return \Syzn\JsonApi\Contracts\MetaInterface|null
+     */
+    public function getMeta(): ?MetaInterface;
 
     // TODO add source
 }
