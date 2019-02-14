736def596faa3f9fe4aba894e1c8312a4617ae25
Arad Zaltsberger <arad@syzn.co.il>
2019-02-14 11:23:37 +0200

    ResourceIdentifierInterface now extends BaseResourceInterface

--- src/Contracts/ResourceIdentifierInterface.php
+++ src/Contracts/ResourceIdentifierInterface.php
@@ -2,21 +2,9 @@
 
 namespace Syzn\JsonApi\Contracts;
 
-use Syzn\JsonApi\Contracts\EncodableJsonApiStructureInterface;
+use Syzn\JsonApi\Contracts\Resources\BaseResourceInterface;
 
-interface ResourceIdentifierInterface extends EncodableJsonApiStructureInterface
+interface ResourceIdentifierInterface extends BaseResourceInterface
 {
-    /**
-     * Retrieve resource type
-     *
-     * @return string
-     */
-    public function getType(): string;
-
-    /**
-     * Retrieve resource identifier
-     *
-     * @return string
-     */
-    public function getIdentifier(): string;
+    //
 }
