e17c7bd465ff81b2f14e7e88ae79506475b77a05
Arad Zaltsberger <arad@syzn.co.il>
2019-02-18 11:50:05 +0200

    Add LinksFactory tests

---
+++ tests/LinksFactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Syzn\JsonApi\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Syzn\JsonApi\Factories\Links\LinkFactory;
+use Syzn\JsonApi\Factories\Links\LinksFactory;
+use Syzn\JsonApi\Links\Link;
+
+final class LinksFactoryTest extends TestCase
+{
+    public function testCreateWithSelf()
+    {
+        $self = LinkFactory::create("/articles/1/relationships/comments");
+
+        $links = LinksFactory::create($self);
+
+        $this->assertEquals(
+            $links->getSelf(),
+            $self
+        );
+    }
+
+    public function testCreateWithSelfAndRelated()
+    {
+        $self = LinkFactory::create("/articles/1/relationships/comments");
+        $related = LinkFactory::create("/articles/1/comments");
+
+        $links = LinksFactory::create(
+            $self,
+            $related
+        );
+
+        $this->assertEquals(
+            $links->getSelf(),
+            $self
+        );
+
+        $this->assertEquals(
+            $links->getRelated(),
+            $related
+        );
+    }
+}
