837cbb7465fe9b2ad8e2d12cc3991e61bf314d7b
Arad Zaltsberger <arad@syzn.co.il>
2019-02-19 12:12:16 +0200

    Rename to QueriesMappingInterface and add doc blocks

---
+++ src/Contracts/Resources/QueriesMappingInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Syzn\JsonApi\Contracts\Resources;
+
+interface QueriesMapInterface
+{
+    /**
+     * Get the allowed filtering properties.
+     *
+     * @return array
+     */
+    public function getFilterable(): array;
+
+    /**
+     * Get the allowed sort fields.
+     *
+     * @return array
+     */
+    public function getSortable(): array;
+
+    /**
+     * Get the fields that may be requested restrictedly.
+     *
+     * @return array
+     */
+    public function getSparsableFields(): array;
+
+    /**
+     * Check whether fetching a collection of this resource is paginated.
+     *
+     * @return array
+     */
+    public function paginatesCollection(): bool;
+}
